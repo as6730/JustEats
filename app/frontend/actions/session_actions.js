a015010a5469f9c84b53796bac7f50cfa43a9e64
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-03 16:43:49 -0700

    Create session actions

---
+++ app/frontend/actions/session_actions.js
@@ -0,0 +1,26 @@
+import * from APIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+export const signup = (user) => dispatch => (
+  APIUtil.login(user).then(user => dispatch(receiveCurrentUser(user)))
+);
+
+export const login = (user) => dispatch => (
+  APIUtil.login(user).then(user => dispatch(receiveCurrentUser(user)))
+);
+
+export const logout = () => dispatch => (
+  APIUtil.login().then(user => dispatch(receiveCurrentUser(null)))
+);
