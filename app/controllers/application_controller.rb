0ff5b728a4c9efb40b42eff8faa40a390cc56e8a
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-03 14:20:45 -0700

    Fix user bcyrpt and complete application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  helper_method :user_logged_in?
+
+  private
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_user
+    redirect_to new_session_url unless current_user
+  end
+
+  def user_logged_in?
+    !!current_user
+  end
+
+  def login_user(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout_user
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
-  validates :password_digest, presence: true
+  validates :password_digest, :firstname, :lastname, presence: true
   validates :session_token, presence: true, uniqueness: true
   after_initialize :ensure_session_token
 
@@ -13,12 +13,12 @@ class User < ApplicationRecord
   attr_reader :password
 
   def is_password?(password)
-    BCrypt::Password.create(self.password_digest).is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
   def password=(password)
     @password = password
-    self.password_digest = BCrypt::Password.new(password)
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def reset_session_token!
