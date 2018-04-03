518a5704f0d27d6e9ebfc35baa4c8a7881d6e50f
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-03 15:31:59 -0700

    Add sessions api controller and routes

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password],
+    )
+
+    if @user
+      login_user(@user)
+      render "api/users"
+    else
+      render json: ['Invalid username/password combination'], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+
+    if @user
+      logout_user
+      render "api/users"
+    else
+      render json: ['User is not signed in'], status: 404
+    end
+  end
+end
