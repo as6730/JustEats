2fd9550178997a4f30b4b47314f14fd1bec9d02a
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-03 18:04:37 -0700

    Create greeting component

---
+++ app/frontend/components/greeting/greeting_container.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { connect } from "react-redux";
+import { greeting } from "./greeting";
+
+export const mapStateToProps = () => ({});
--- app/frontend/entry.jsx
+++ app/frontend/entry.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
   const root = document.getElementById("root");
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  ReactDOM.render(<h1>Welcome to JustEat</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
