614470bef6bfe33f9f39a0a672ead3c2274abc44
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-04 18:58:50 -0700

    Create restaurant favorite join table migration

---
+++ db/migrate/20180405015732_create_restaurant_favorites.rb
@@ -0,0 +1,13 @@
+class CreateRestaurantFavorites < ActiveRecord::Migration[5.1]
+  def change
+    create_table :restaurant_favorites do |t|
+      t.integer :user_id, null: false
+      t.integer :restaurant_id, null: false
+
+      t.timestamps
+    end
+
+    add_index, :restaurant_favorites, :user_id
+    add_index, :restaurant_favorites, :restaurant_id
+  end
+end
---
+++ test/models/restaurant_favorite_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RestaurantFavoriteTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
