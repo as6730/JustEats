86a7fc742616457c97f4e124481afd3c6b821393
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-04 19:43:16 -0700

    Migrate restaurants, reviews, users, menus, payment, tags, etc

--- db/migrate/20180405015044_create_restaurant_payment_options.rb
+++ db/migrate/20180405015044_create_restaurant_payment_options.rb
@@ -1,13 +1,13 @@
 class CreateRestaurantPaymentOptions < ActiveRecord::Migration[5.1]
   def change
-    create_table :restaurant_payment_options do |t|
+    create_table :restaurant_payment do |t|
       t.integer :payment_option_id, null: false
       t.integer :restaurant_id, null: false
 
       t.timestamps
     end
 
-    add_index(:restaurant_payment_options, [:payment_option_id, :restaurant_id], unique: true)
-    add_index :restaurant_payment_options, :restaurant_id
+    add_index(:restaurant_payment, [:payment_option_id, :restaurant_id], unique: true)
+    add_index :restaurant_payment, :restaurant_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,155 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404231613) do
+ActiveRecord::Schema.define(version: 20180405020000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cuisines", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_cuisines_on_name", unique: true
+  end
+
+  create_table "dishes", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "price", null: false
+    t.string "description"
+    t.integer "menu_section_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["menu_section_id"], name: "index_dishes_on_menu_section_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.float "latitude", null: false
+    t.float "longitude", null: false
+    t.string "address"
+    t.string "neighborhood"
+    t.string "cross_street"
+    t.string "parking_details"
+    t.string "location_image_url"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_locations_on_restaurant_id"
+  end
+
+  create_table "menu_sections", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "menu_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["menu_id"], name: "index_menu_sections_on_menu_id"
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name", null: false
+    t.date "last_update", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
+  end
+
+  create_table "payment_options", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_payment_options_on_name", unique: true
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "url", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_photos_on_restaurant_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer "party_size", null: false
+    t.date "date", null: false
+    t.integer "points", null: false
+    t.integer "user_id", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reservations_on_restaurant_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "restaurant_cuisines", force: :cascade do |t|
+    t.integer "cuisine_id", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cuisine_id", "restaurant_id"], name: "index_restaurant_cuisines_on_cuisine_id_and_restaurant_id", unique: true
+    t.index ["restaurant_id"], name: "index_restaurant_cuisines_on_restaurant_id"
+  end
+
+  create_table "restaurant_favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_restaurant_favorites_on_restaurant_id"
+    t.index ["user_id", "restaurant_id"], name: "index_restaurant_favorites_on_user_id_and_restaurant_id", unique: true
+  end
+
+  create_table "restaurant_payment", force: :cascade do |t|
+    t.integer "payment_option_id", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["payment_option_id", "restaurant_id"], name: "index_restaurant_payment_on_payment_option_id_and_restaurant_id", unique: true
+    t.index ["restaurant_id"], name: "index_restaurant_payment_on_restaurant_id"
+  end
+
+  create_table "restaurant_tags", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_restaurant_tags_on_restaurant_id"
+    t.index ["tag_id", "restaurant_id"], name: "index_restaurant_tags_on_tag_id_and_restaurant_id", unique: true
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description", null: false
+    t.string "phone_number"
+    t.string "website_link"
+    t.string "hours"
+    t.string "dining_style"
+    t.string "dress_code"
+    t.string "executive_chef"
+    t.string "price_range"
+    t.string "private_party_facilities"
+    t.string "private_party_contact"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "username"
+    t.string "body"
+    t.integer "rating"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
