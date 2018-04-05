3fb771f5b389f63a6d59ace6c488b9cfbd2b70cf
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-05 16:02:01 -0700

    Add review routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,17 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'reviews/new'
-  end
-
-  namespace :api do
-    get 'reviews/index'
-  end
-
-  namespace :api do
-    get 'reviews/create'
-  end
-
-  namespace :api do
-    get 'reviews/update'
-  end
-
-  namespace :api do
-    get 'reviews/edit'
-  end
-
-  namespace :api do
-    get 'reviews/show'
-  end
-
-  namespace :api do
-    get 'reviews/destroy'
-  end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
+
+    resources :reviews, only: [:edit, :destroy]
     resource :session, only: [:create, :destroy]
     resources :users, only: [:create, :show, :edit]
+    resources :reservations, only: [:edit, :show, :destroy]
 
     resources :restaurants do
+      resources :reviews, only: [:index, :create, :show]
+      resources :reservations, only: [:create]
     end
-
-    resources :reservations, only: [:edit, :show, :destroy]
   end
 end
