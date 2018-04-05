00400ae77b834af65863a3107124bbf4409e1b1f
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-04 19:27:50 -0700

    Add null false to columns inrestaurant cuisines join table

--- db/migrate/20180405014730_create_restaurant_cuisines.rb
+++ db/migrate/20180405014730_create_restaurant_cuisines.rb
@@ -1,8 +1,8 @@
 class CreateRestaurantCuisines < ActiveRecord::Migration[5.1]
   def change
     create_table :restaurant_cuisines do |t|
-      t.integer :cuisine_id
-      t.integer :restaurant_id
+      t.integer :cuisine_id, null: false
+      t.integer :restaurant_id, null: false
 
       t.timestamps
     end
