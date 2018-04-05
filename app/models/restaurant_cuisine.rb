0eec7b4b86047bb74dbb231b1b4d15523d719164
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-04 18:49:04 -0700

    Create restaurant cuisine join table migration

---
+++ app/models/restaurant_cuisine.rb
@@ -0,0 +1,2 @@
+class RestaurantCuisine < ApplicationRecord
+end
---
+++ db/migrate/20180405014730_create_restaurant_cuisines.rb
@@ -0,0 +1,13 @@
+class CreateRestaurantCuisines < ActiveRecord::Migration[5.1]
+  def change
+    create_table :restaurant_cuisines do |t|
+      t.integer :cuisine_id
+      t.integer :restaurant_id
+
+      t.timestamps
+    end
+
+    add_index, :restaurant_cuisines, :cuisine_id
+    add_index, :restaurant_cuisines, :restaurant_id
+  end
+end
