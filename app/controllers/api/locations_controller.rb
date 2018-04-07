a493c77a8732ac2824ef88ea0353c3416204ffe8
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-04-07 15:09:30 -0700

    Create location controller

---
+++ app/controllers/api/locations_controller.rb
@@ -0,0 +1,41 @@
+class Api::LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+  end
+
+  def create
+    @location = Location.new(JSON.parse(request.body.read))
+
+    if @location.save
+      render :show
+    else
+      render json: @location.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @location = Location.find(params[:id])
+
+    if @location.update_attributes(location_params)
+      render :show
+    else
+      render json: @location.errors.full_messages, status: 404
+    end
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def destroy
+    location = Location.find(params[:id])
+    location.destroy!
+    render json: "{\"status\" : \"success\"}"
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:latitude, :longitude)
+  end
+end
