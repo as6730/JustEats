d4ca851a7eac326090f4fa965614a611e2bf756f
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-05-08 15:57:31 -0700

    Clean up footer for main page

--- frontend/components/restaurant/home_page.jsx
+++ frontend/components/restaurant/home_page.jsx
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import FeaturedPhotos from "./featured_photos";
 import HomePageSlickr from "./home_page_slickr";
 import { Link } from "react-router-dom";
-import IconGithub from 'react-icons/lib/fa/github-square';
-import IconLinkedIn from 'react-icons/lib/fa/linkedin-square';
+import IconGithub from 'react-icons/lib/fa/github';
+import IconLinkedIn from 'react-icons/lib/fa/linkedin';
+import IconAngellist from 'react-icons/lib/fa/angellist';
 
 class HomePage extends Component {
   constructor(props) {
@@ -55,12 +56,9 @@ class HomePage extends Component {
         <footer className="footer">
           <div className="footer-info">
             Alexandra Savramis
-          </div>
-          <div className="footer-info">
-            <a href="https://github.com/as6730">Github Link: {<IconGithub className="github-icon" size={30}/>}</a>
-          </div>
-          <div className="footer-info">
-            <a href="https://www.linkedin.com/in/alexandrasavramis/">LinkedIn Profile: {<IconLinkedIn className="linkedin-icon" size={30}/>}</a>
+            <a href="https://angel.co/alexandra-savramis">{<IconAngellist className="angellist-icon" size={15}/>}</a>
+            <a href="https://www.linkedin.com/in/alexandrasavramis/">{<IconLinkedIn className="linkedin-icon" size={20}/>}</a>
+            <a href="https://github.com/as6730">{<IconGithub className="github-icon" size={20}/>}</a>
           </div>
         </footer>
       </div>
